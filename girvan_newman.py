8a1122985061274688750e80a822bc8c5104b2b3
Raphaël Margueron <raphael.margueron@he-arc.ch>
2020-04-01 16:01:29 +0200

    add requirements + add gn vizualisation + refactor gn module
    create a vizualisation for the gn algorithm in usecase

--- girvan_newman.py
+++ girvan_newman.py
@@ -1,6 +1,6 @@
+#!/usr/bin/env python
 import itertools
 import networkx as nx
-from tools import Timer
 from collections import defaultdict
 
 
@@ -13,24 +13,7 @@ def girvan_newman(G):
 
     # iterate while its still possible to remove edges
     while len(G.edges) > 0:
-        # calculate all the shortest path for every combination of nodes
-        # needed for the edge_betweeness computation
-
-        all_shortest_paths = {}
-        for vi, vj in itertools.combinations(G.nodes, 2):
-            try:
-                paths = list(nx.all_shortest_paths(G, vi, vj))
-                all_shortest_paths[(vi, vj)] = paths
-            except nx.exception.NetworkXNoPath:
-                all_shortest_paths[(vi, vj)] = []
-
-        # compute dictionary needed by edge betweenness
-        counted_paths = count_paths(all_shortest_paths, G.edges)
-
-        # evaluate the "weekness" of the edges of the graph using
-        # edge_betweeness algorithm
-        edge_betweenness_dict = {edge: edge_betweenness(
-            counted_paths, edge) for edge in G.edges}
+        edge_betweenness_dict = get_edge_betweenness_dict(G)
 
         # take the edge with the highest edge_betweenness score
         edge_to_remove = max(edge_betweenness_dict.keys(),
@@ -48,27 +31,43 @@ def girvan_newman(G):
         if new_number_of_communities > current_number_of_communities:
             # set the current number of communities to the new value
             current_number_of_communities = new_number_of_communities
+            # yield the communities for the current level of iteration
+            communities = nx.connected_components(G)
+            yield tuple(communities)
 
-            yield tuple(nx.connected_components(G))
 
-
-def count_paths(all_shortest_paths, edges):
-    """Return a dictionary containing for every shortest path for all
-    combination of endpoints:
-    - The number of paths
-    - For each edge : the number of path involved
+def get_edge_betweenness_dict(G):
+    """Calculate the edge betweeness score for every edges of the graph
     """
-    count_paths = {}
+    # calculate all the shortest path for every combination of nodes
+    all_shortest_paths = {}
+    for vi, vj in itertools.combinations(G.nodes, 2):
+        try:
+            paths = list(nx.all_shortest_paths(G, vi, vj))
+            all_shortest_paths[(vi, vj)] = paths
+        except nx.exception.NetworkXNoPath:
+            all_shortest_paths[(vi, vj)] = []
+
+    # compute the counted path dictionary
+    # a dictionary containing for every shortest path for every
+    # combination of endpoints:
+    counted_paths = {}
     for (start, end), paths in all_shortest_paths.items():
         nb_paths = len(paths)
-        counted_paths = defaultdict(lambda: 0)
+        counted_paths_through_edge = defaultdict(lambda: 0)
         for path in paths:
             edges_pathlr = list(zip(path[:-1], path[1:]))
             for l, r in edges_pathlr:
-                counted_paths[(l, r)] += 1
-                counted_paths[(r, l)] += 1
-        count_paths[(start, end)] = (nb_paths, counted_paths)
-    return count_paths
+                counted_paths_through_edge[(l, r)] += 1
+                counted_paths_through_edge[(r, l)] += 1
+        counted_paths[(start, end)] = (nb_paths, counted_paths_through_edge)
+
+    # evaluate the "weekness" of the edges of the graph using
+    # edge_betweeness algorithm
+    edge_betweenness_dict = {edge: edge_betweenness(
+        counted_paths, edge) for edge in G.edges}
+
+    return edge_betweenness_dict
 
 
 def edge_betweenness(counted_paths, edge):
--- girvan_newman_usecase.py
+++ girvan_newman_usecase.py
@@ -1,21 +1,22 @@
-import networkx as nx
-from girvan_newman import girvan_newman
+#!/usr/bin/env python
 import itertools
-from tools import lecture_graph
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from girvan_newman import girvan_newman
 
 
-def analysis():
-    # G = nx.read_edgelist('subgraph.gz')
-    # wip
-    G = lecture_graph()
+def analysis(G):
+    print("Starting analysis")
+    max_iteration_level = 4
+
     gn_iterator = girvan_newman(G)
-
-    max_iteration_level = 10
+    gn_iterator = itertools.islice(gn_iterator, max_iteration_level)
 
     communities_ = {}
-    for communities in enumerate(itertools.islice(gn_iterator, max_iteration_level)):
+    for i, communities in enumerate(gn_iterator):
         communities_[len(communities)] = communities
-        print(communities)
+        print(f"Iteration {i}")
         print_infos(communities)
 
     return communities_
@@ -31,11 +32,35 @@ def print_infos(communities):
     print(f"Size of the biggest of community : {len(communities[-1])}")
 
 
-def vizualisation(communities):
-    pass
-    # todo
+def vizualisation(G, communities):
+    print("Starting vizualisation")
+    for level, communities_level in communities.items():
+        plt.figure()
+        plt.title(f"Iteration: {level} ({len(communities_level)} communities)")
+
+        # create a table with for every nodes an id
+        # with respect to it's community
+        colors = []
+        for node in G.nodes:
+            for i, community in enumerate(communities_level):
+                if node in community:
+                    colors.append(i)
+
+        # draw the graph with the nodes colored in the community color and
+        # the edges
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_color=colors, cmap="brg")
+        nx.draw_networkx_edges(G, pos)
+    plt.show()
+
+
+def main():
+    G = nx.read_edgelist('subgraph.gz')
+    # from tools import lecture_graph
+    # G = lecture_graph()
+    communities = analysis(G)
+    vizualisation(G, communities)
 
 
 if __name__ == '__main__':
-    communities = analysis()
-    vizualisation(communities)
+    main()
